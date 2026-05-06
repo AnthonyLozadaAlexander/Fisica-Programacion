e42012d1a583cd7ab770a0974cb65c3885542c7c
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-06 02:01:21 -0500

    Agrega archivo para simulación de movimiento rectilíneo uniforme acelerado
    Este commit introduce un nuevo archivo que establece el tiempo de simulación y define las variables iniciales para la simulación de movimiento rectilíneo uniforme acelerado. Se utilizan bibliotecas de numpy y matplotlib para facilitar el cálculo y la visualización.

---
+++ Conceptos/movimiento_rectilineo_uniforme_acelerado.py
@@ -0,0 +1,10 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+
+# Creamos el tiempo de nuestra simulacion
+# 100 fps espaciados uniformemente entre 0 y 10 segundos
+t = np.linspace(0, 10, 100)
+
+posicion_inicial = 0
+velocidad_inicial = 0
+aceleracion = 2
