f36688d0d70bceb4fd8c7ca8d8d6ece982b83ff5
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-06 02:28:33 -0500

    Agrega archivo para simulación de movimiento rectilíneo uniforme acelerado
    Este commit introduce un nuevo archivo que establece el tiempo de simulación y define las variables iniciales para la simulación de movimiento rectilíneo uniforme acelerado. Se utilizan bibliotecas de numpy y matplotlib para facilitar el cálculo y la visualización.

--- Conceptos/movimiento_rectilineo_uniforme_acelerado.py
+++ Conceptos/movimiento_rectilineo_uniforme_acelerado.py
@@ -8,3 +8,11 @@ t = np.linspace(0, 10, 100)
 posicion_inicial = 0
 velocidad_inicial = 0
 aceleracion = 2
+
+posicion = posicion_inicial + (velocidad_inicial * t) + 1/2 * (aceleracion*(1**2))
+
+plt.plot(t, color = "red", linewidth = 2)
+plt.tittle("Simulacion MRUA: Aumento VVelerado")
+plt.xlabel = "tiempo y segundos)")
+plt.ylabel = "Posicion (Metros)")
+plt.grid(Tr
