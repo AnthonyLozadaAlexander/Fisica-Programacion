9c1d02b596cf8e7bdc4a3f4fdf31fb8434fd610b
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-08 01:27:18 -0500

    Corrige comentario sobre el ancho de línea en la gráfica

--- Ejercicios/MRU_E1.py
+++ Ejercicios/MRU_E1.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 # Un tren de carga viaja en línea recta manteniendo una velocidad constante de 85 km/h. ¿Qué distancia exacta, expresada en kilómetros, habrá recorrido después de 3 horas de viaje continuo?
 # Datos: v = 85, t = 3
 
-t = np.linspace(0, 3, 100)
+t = np.linspace(0, 3, 100) # Tiempo desde 0 hasta 3 horas
 v = 85
 d = v*t
 posicion = v * t
 
-plt.plot(t, posicion, color = "blue", linewidth = 2)
+plt.plot(t, posicion, color = "blue", linewidth = 2) # linediwth es
 
 plt.title("Simulacion de Posicion")
 plt.xlabel("tiempo (Horas)")
