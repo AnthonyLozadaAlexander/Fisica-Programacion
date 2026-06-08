84af54ab5663025425b0df2132b0f89532fea18a
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-08 01:46:14 -0500

    Mejora la legibilidad de los comentarios y corrige un error tipográfico en el código de simulación de movimiento rectilíneo uniforme.

--- Ejercicios/MRU_E1.py
+++ Ejercicios/MRU_E1.py
@@ -2,7 +2,11 @@
 import matplotlib.pyplot as plt
 
 # Ejercicio 1: Cálculo de Distancia (Nivel Básico)
-# Un tren de carga viaja en línea recta manteniendo una velocidad constante de 85 km/h. ¿Qué distancia exacta, expresada en kilómetros, habrá recorrido después de 3 horas de viaje continuo?
+# Un tren de carga viaja en línea recta manteniendo 
+# una velocidad constante de 85 km/h. 
+# ¿Qué distancia exacta, expresada en kilómetros, 
+# habrá recorrido después de 3 horas de viaje continuo?
+
 # Datos:
 # v = 85,
 # t = 3
@@ -12,7 +16,8 @@ v = 85
 d = v*t
 posicion = v * t
 
-plt.plot(t, posicion, color = "blue", linewidth = 2) # linediwth es
+plt.plot(t, posicion, color = "blue", linewidth = 2) 
+# linewidt es el grosor de la linea
 
 plt.title("Simulacion de Posicion")
 plt.xlabel("tiempo (Horas)")
@@ -20,6 +25,7 @@ plt.ylabel("Posicion (Kilometros)")
 plt.grid(True)
 plt.show()
 
-print(f"La distancia recorrida es de: {d[-1]}") # accede al ultimo elemento del arreglo que almacena la distancia total
+print(f"La distancia recorrida es de: {d[-1]}") 
+# accede al ultimo elemento del arreglo que almacena la distancia total
 
 
