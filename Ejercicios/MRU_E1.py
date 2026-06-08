3b5e223b71bf305f861168d1435d298d157e525a
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-08 01:42:32 -0500

    Corrige datos de entrada y mejora impresión de distancia recorrida

--- Ejercicios/MRU_E1.py
+++ Ejercicios/MRU_E1.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 
 # Ejercicio 1: Cálculo de Distancia (Nivel Básico)
 # Un tren de carga viaja en línea recta manteniendo una velocidad constante de 85 km/h. ¿Qué distancia exacta, expresada en kilómetros, habrá recorrido después de 3 horas de viaje continuo?
-# Datos: v = 85, t = 3
+# Datos:
+# v = 85,
+# t = 3
 
 t = np.linspace(0, 3, 100) # Tiempo desde 0 hasta 3 horas
 v = 85
@@ -18,6 +20,6 @@ plt.ylabel("Posicion (Kilometros)")
 plt.grid(True)
 plt.show()
 
-print(f"La distancia recorrida es de: {d}")
+print(f"La distancia recorrida es de: {d[-1]}") # accede al ultimo elemento del arreglo que almacena la distancia total
 
 
