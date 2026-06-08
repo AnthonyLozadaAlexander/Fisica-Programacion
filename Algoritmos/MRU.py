6a620e1496b71f4bd405d2dc977fc2cc2a09d06a
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-08 02:58:57 -0500

    Agrega funciones para calcular velocidad, distancia y tiempo
    Se implementan tres funciones: velocidad, distancia y tiempo, que permiten calcular estos valores en función de los parámetros de entrada. Se incluyen validaciones para evitar resultados no válidos.

---
+++ Algoritmos/MRU.py
@@ -0,0 +1,23 @@
+﻿
+def velocidad(d, t):
+    if(d < 0 or t <= 0):
+        return 0
+    else:
+        v = (d)/(t)
+        
+    return v
+
+def distancia(v, t):
+    if(v < 0 or t <= 0):
+        return 0
+    else:
+        d = (v)*(t)
+    return d
+
+def tiempo(d, v):
+    if(d < 0 or v <= 0):
+        return 0
+    else:
+        t = (d)/(v)
+    return t
+
