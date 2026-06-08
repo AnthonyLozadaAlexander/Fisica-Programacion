e18a422ed8e528432cc621826a3a2b42989af415
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-08 01:24:30 -0500

    Agrega simulación de movimiento rectilíneo uniforme
    Implementa un script para calcular y graficar la posición de un tren de carga que viaja a una velocidad constante de 85 km/h durante 3 horas. Se incluye la visualización de la posición en función del tiempo y se imprime la distancia recorrida.

---
+++ Ejercicios/MRU_E1.py
@@ -0,0 +1,23 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+
+# Ejercicio 1: Cálculo de Distancia (Nivel Básico)
+# Un tren de carga viaja en línea recta manteniendo una velocidad constante de 85 km/h. ¿Qué distancia exacta, expresada en kilómetros, habrá recorrido después de 3 horas de viaje continuo?
+# Datos: v = 85, t = 3
+
+t = np.linspace(0, 3, 100)
+v = 85
+d = v*t
+posicion = v * t
+
+plt.plot(t, posicion, color = "blue", linewidth = 2)
+
+plt.title("Simulacion de Posicion")
+plt.xlabel("tiempo (Horas)")
+plt.ylabel("Posicion (Kilometros)")
+plt.grid(True)
+plt.show()
+
+print(f"La distancia recorrida es de: {d}")
+
+
